513037eaa8b60c9fc4c2699142b6e1d3b108544f
Chall <a.ahmedtws23173@gmail.com>
2025-03-17 16:00:53 +0100

    Enabled suggestions module

--- listeners/suggestions.js
+++ listeners/suggestions.js
@@ -1,36 +1,38 @@
-// const { Listener } = require('gcommands');
-// const config = require('../config.json');
+const { Listener } = require("gcommands");
+const config = require("../config.json");
 
-// new Listener({
-//     name: 'Suggestion Send',
-//     event: 'messageCreate',
+new Listener({
+  name: "Suggestion Send",
+  event: "messageCreate",
 
-//     run: async ctx => {
-//         if (ctx.author.bot) return;
-//         console.log(ctx.channel.id === config.discord.suggestionsChannelId)
-//         if (ctx.channel.id === config.discord.suggestionsChannelId) {
-//             // Check if message starts a thread
-//             if (ctx.thread) return;
-//             try {
-//                 await ctx.delete();
-//                 const message = await ctx.channel.send({
-//                     embeds: [{
-//                         description: ctx.content,
-//                         color: 0xFFFFFF,
-//                         footer: {
-//                             text: 'Upvote with ✅. Downvote with ❌.'
-//                         },
-//                         author: {
-//                             name: ctx.author.username,
-//                             icon_url: ctx.author.displayAvatarURL()
-//                         }
-//                     }]
-//                 });
-//                 message.react('✅');
-//                 message.react('❌');
-//             } catch (error) {
-//                 console.error('Error sending suggestion:', error);
-//             }
-//         }
-//     }
-// })
+  run: async (ctx) => {
+    if (ctx.author.bot) return;
+    console.log(ctx.channel.id === config.discord.suggestionsChannelId);
+    if (ctx.channel.id === config.discord.suggestionsChannelId) {
+      // Check if message starts a thread
+      if (ctx.thread) return;
+      try {
+        await ctx.delete();
+        const message = await ctx.channel.send({
+          embeds: [
+            {
+              description: ctx.content,
+              color: 0xffffff,
+              footer: {
+                text: "Upvote with ✅. Downvote with ❌.",
+              },
+              author: {
+                name: ctx.author.username,
+                icon_url: ctx.author.displayAvatarURL(),
+              },
+            },
+          ],
+        });
+        message.react("✅");
+        message.react("❌");
+      } catch (error) {
+        console.error("Error sending suggestion:", error);
+      }
+    }
+  },
+});
